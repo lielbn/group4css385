e4a82ef8059a5277ddd5a4e08ce5f94d7784d886
lielbn <lielbn@uw.edu>
2025-05-11 21:26:07 -0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,27 +4,18 @@ import Link from 'next/link'
 
 export default function Navbar() {
   return (
-    <nav className="w-full bg-[#c4743b] text-white p-4 shadow-md font-game flex justify-between items-center">
-      <div className="text-lg tracking-wide uppercase">
-        🏜️ To-Get-Her
+    <nav className="w-full bg-[#643a28] text-white p-4 shadow-md font-game flex justify-center">
+      <div className="flex space-x-10 uppercase text-sm tracking-wider">
+        <Link href="/">
+          <span className="hover:text-[#fdco94] transition">Home</span>
+        </Link>
+        <Link href="#play">
+          <span className="hover:text-[#fdco94] transition">Play Game</span>
+        </Link>
+        <Link href="#theme">
+          <span className="hover:text-[#fdco94] transition">Theme</span>
+        </Link>
       </div>
-      <ul className="flex space-x-6 text-sm">
-        <li>
-          <Link href="/">
-            <span className="hover:text-yellow-300 transition">Home</span>
-          </Link>
-        </li>
-        <li>
-          <Link href="#play">
-            <span className="hover:text-yellow-300 transition">Play Game</span>
-          </Link>
-        </li>
-        <li>
-          <Link href="#theme">
-            <span className="hover:text-yellow-300 transition">Theme</span>
-          </Link>
-        </li>
-      </ul>
     </nav>
   )
 }
