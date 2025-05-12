ea91b58361c90c60c6cf2b2b595b9691a5091524
lielbn <lielbn@uw.edu>
2025-05-11 21:54:49 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image'
 import Link from 'next/link'
-import Navbar from '../components/Navbar.tsx'
+import Navbar from '../components/Navbar'
 
 
 export default function GamePage() {
