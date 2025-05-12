9ef3a3a5e9faf5b21eaf79203c8b54edc439148d
lielbn <lielbn@uw.edu>
2025-05-12 09:47:33 -0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,26 +7,26 @@ export default function Navbar() {
     <nav className="w-full bg-[#643a28] text-white px-8 py-4 shadow-md font-game z-50">
       <div className="max-w-7xl mx-auto flex items-center justify-between">
         {/* Logo / Title */}
-        <div className="text-lg tracking-wider uppercase text-[#fdco94]">
+        <div className="text-lg tracking-wider uppercase text-[#fdc094]">
           To-Get-Her
         </div>
 
         {/* Navigation Links */}
         <div className="flex space-x-8 text-sm uppercase tracking-wide">
-          <Link href="/">
-            <span className="text-[#fcebd6] hover:text-[#c4743b] transition duration-200">
+          <Link href="/" passHref>
+            <a className="text-[#fcebd6] hover:text-[#c4743b] transition duration-200">
               Home
-            </span>
+            </a>
           </Link>
-          <Link href="#play">
-            <span className="text-[#fcebd6] hover:text-[#c4743b] transition duration-200">
+          <Link href="#play" passHref>
+            <a className="text-[#fcebd6] hover:text-[#c4743b] transition duration-200">
               Play Game
-            </span>
+            </a>
           </Link>
-          <Link href="#theme">
-            <span className="text-[#fcebd6] hover:text-[#c4743b] transition duration-200">
+          <Link href="#theme" passHref>
+            <a className="text-[#fcebd6] hover:text-[#c4743b] transition duration-200">
               Theme
-            </span>
+            </a>
           </Link>
         </div>
       </div>
