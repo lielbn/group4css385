5ed87a365c7c4ab588fc344e9cecf86a3b630a18
lielbn <lielbn@uw.edu>
2025-05-12 09:38:08 -0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,10 @@
 'use client'
 
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+export default function Navbar() {
+  const pathname = usePathname()
 
 export default function Navbar() {
   return (
